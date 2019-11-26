0fd37bd3ea808d1cf4c6f5c5ad9ca3ab20d7b6cf
Jakab Gabor <jkbgbr@gmail.com>
2019-11-26 22:35:01 +0100

    updated log descriptions,

--- LogViewer/source/log_definition.py
+++ LogViewer/source/log_definition.py
@@ -3,24 +3,25 @@
 log definitions
 """
 
-# pnp = {'entry_structure': ('Timestamp', 'Session', 'Level', 'emitter', 'module', 'line', 'method', 'message'),
-#        'separator': ' -- ',
-#        'name': 'PNP',
-#        'directory_path': 'C:\\Users\\Jakab Gábor\\AppData\\Roaming\\LogViewer\\Logs'}
-#
-# vw = {'entry_structure': ('Timestamp', 'Session', 'Level', 'emitter', 'module', 'line', 'method', 'message'),
-#       'separator': ' ++ ',
-#       'name': 'VW',
-#       'directory_path': 'C:\\Users\\Jakab Gábor\\AppData\\Roaming\\LogViewer\\Logs'}
+pnp = {'entry_structure': ('Timestamp', 'Session', 'emitter', 'Level', 'message'),
+       'separator': ' - ',
+       'name': 'PNP',
+       'directory_path': 'V:\\KO\\NozzlePro'}
 
 wss = {'entry_structure': ('Timestamp', 'Session', 'Level', 'message'),
        'separator': ' -- ',
        'name': 'WSS',
        'directory_path': 'W:\\GJ\\Logs\\WSS'}
 
+#
 # vw = {'entry_structure': ('Timestamp', 'Session', 'Level', 'emitter', 'module', 'line', 'method', 'message'),
 #       'separator': ' ++ ',
 #       'name': 'VW',
 #       'directory_path': 'C:\\Users\\Jakab Gábor\\AppData\\Roaming\\LogViewer\\Logs'}
 
-logdefinitions = wss,
+# vw = {'entry_structure': ('Timestamp', 'Session', 'Level', 'emitter', 'module', 'line', 'method', 'message'),
+#       'separator': ' ++ ',
+#       'name': 'VW',
+#       'directory_path': 'C:\\Users\\Jakab Gábor\\AppData\\Roaming\\LogViewer\\Logs'}
+
+logdefinitions = wss, pnp
