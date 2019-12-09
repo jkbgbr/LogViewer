7f49cbad21540e832c2db5009f867d768da6483d
Jakab Gabor <jkbgbr@gmail.com>
2019-12-09 10:29:56 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='LogViewer',
-    version='0.0.1',
+    version='0.0.3',
     packages=['LogViewer', 'LogViewer.docs', 'LogViewer.source'],
     url='',
     license='MIT',
